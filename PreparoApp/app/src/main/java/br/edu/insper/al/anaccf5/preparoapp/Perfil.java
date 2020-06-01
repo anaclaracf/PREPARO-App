d56f2c4c5d6b2d69fba66820e936419ed6561cc5
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2020-05-31 22:35:03 -0300

    navbar

--- PreparoApp/app/src/main/java/br/edu/insper/al/anaccf5/preparoapp/Perfil.java
+++ PreparoApp/app/src/main/java/br/edu/insper/al/anaccf5/preparoapp/Perfil.java
@@ -1,21 +1,28 @@
 package br.edu.insper.al.anaccf5.preparoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Perfil extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     private FirebaseUser user;
-
     private TextView user_email, le_id;
     private Button log_out;
 
@@ -23,8 +30,21 @@ public class Perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-
-        inicializaComponentes();
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+//        inicializaComponentes();
 
         log_out.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,11 +59,13 @@ public class Perfil extends AppCompatActivity {
         });
     }
 
-    private void inicializaComponentes() {
-        user_email = (TextView) findViewById(R.id.email_perfil);
-        le_id = (TextView) findViewById(R.id.id_perfil);
-        log_out = (Button) findViewById(R.id.logout);
-    }
+//    @Override
+//    public boolean OnNavigationItemSelected(@NonNull MenuItem item);
+//    private void inicializaComponentes() {
+//        user_email = (TextView) findViewById(R.id.email_perfil);
+//        le_id = (TextView) findViewById(R.id.id_perfil);
+//        log_out = (Button) findViewById(R.id.logout);
+//    }
 
     @Override
     protected void onStart() {
@@ -62,4 +84,5 @@ public class Perfil extends AppCompatActivity {
             le_id.setText("ID: \n".concat(user.getUid() + "\n"));
         }
     }
+
 }

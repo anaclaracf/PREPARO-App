5148b1efb8a91f643ab53bb128518b71808dc1e8
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2020-06-01 20:24:22 -0300

    informaçoes pessoais

---
+++ PreparoApp/app/src/main/java/br/edu/insper/al/anaccf5/preparoapp/InformacoesPessoais.java
@@ -0,0 +1,14 @@
+package br.edu.insper.al.anaccf5.preparoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformacoesPessoais extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacoes_pessoais);
+    }
+}

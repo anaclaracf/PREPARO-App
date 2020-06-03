c7e233d9f199c17d304add1dcd9453d414b0e99a
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2020-06-03 14:46:25 -0300

    curriculo

--- PreparoApp/app/src/main/java/br/edu/insper/al/anaccf5/preparoapp/Curriculo.java
+++ PreparoApp/app/src/main/java/br/edu/insper/al/anaccf5/preparoapp/Curriculo.java
@@ -30,152 +30,197 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.google.firestore.admin.v1beta1.Progress;
 
 public class Curriculo extends AppCompatActivity {
 
-    Button selectFile, upload;
-    TextView notification;
-    Uri pdfUri;
-    FirebaseStorage storage;
-    FirebaseDatabase database;
-//    ProgressDialog progressDialog;
-//
+
+    private Button btnSelect, btnupload;
+
+    private TextView mFileStatus;
+
+    private FirebaseDatabase mDatabase;
+
+    private FirebaseStorage mStorage;
+
+    private Uri pdfUri;
+
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curriculo);
-        Button avancar = findViewById(R.id.continuar);
-        Button voltar = findViewById(R.id.voltar);
 
-        voltar.setOnClickListener(new View.OnClickListener() {
+        btnSelect = findViewById(R.id.botao_curriculo);
 
+        btnupload = findViewById(R.id.botao_upload);
+
+        mFileStatus = findViewById(R.id.mensagem);
+
+        mDatabase = FirebaseDatabase.getInstance();
+
+        mStorage = FirebaseStorage.getInstance();
+
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
-                Intent intent = new Intent(Curriculo.this, Localizacao.class);
-                startActivity(intent);
-                Curriculo.this.onPause();
+                if (ContextCompat.checkSelfPermission(Curriculo.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    selectPDF();
+
+                } else {
+
+                    ActivityCompat.requestPermissions(Curriculo.this, new String[]
+                            {Manifest.permission.READ_EXTERNAL_STORAGE}, 5699);
+
+                }
+
             }
         });
 
-        avancar.setOnClickListener(new View.OnClickListener() {
+        btnupload.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Curriculo.this, Habilidades.class);
-                startActivity(intent);
-                onPause();
+            public void onClick(View v) {
+
+                if (pdfUri != null) {
+
+                    uploadPDF(pdfUri);
+
+                } else {
+
+                    Toast.makeText(Curriculo.this, "Please select a file!", Toast.LENGTH_SHORT).show();
+
+                }
 
             }
-
-            ;
-        });
-
-        storage = FirebaseStorage.getInstance();
-        database = FirebaseDatabase.getInstance();
-        selectFile = findViewById(R.id.botao_curriculo);
-        upload = findViewById(R.id.botao_upload);
-        notification = findViewById(R.id.mensagem);
-        selectFile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(Curriculo.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    selectPdf();
-                } else
-                    ActivityCompat.requestPermissions(Curriculo.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 9);
-            }
-
-        });
-
-        upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (pdfUri != null)
-                uploadFile(pdfUri);
-                else
-                    Toast.makeText(Curriculo.this,"Escolha um arquivo", Toast.LENGTH_SHORT).show();
-            }
-
         });
 
     }
 
-    private void uploadFile(Uri pdfUri) {
-//        progressDialog = new ProgressDialog(this);
-//        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-//        progressDialog.setTitle("Fazendo Upload");
-//        progressDialog.setProgress(0);
-//        progressDialog.show();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        final String fileName = System.currentTimeMillis() + "";
-        StorageReference storageReference = storage.getReference();
-        storageReference.child("Uploads").child(fileName).putFile(pdfUri)
+        if (requestCode == 5699 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            selectPDF();
+
+        } else {
+
+            Toast.makeText(this, "Please allow external storage reading!", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    private void selectPDF() {
+
+        Intent intent = new Intent();
+
+        intent.setType("application/pdf");
+
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(intent, 9965);
+
+    }
+
+    private void uploadPDF(Uri pdfUri) {
+
+        progressDialog = new ProgressDialog(Curriculo.this);
+
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+
+        progressDialog.setTitle("Uploading File...");
+
+        progressDialog.setProgress(0);
+
+        progressDialog.show();
+
+
+        final String fileName = System.currentTimeMillis()+" ";
+
+        StorageReference mySto = mStorage.getReference();
+
+        mySto.child("Uploads").child(fileName).putFile(pdfUri)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
                         Task<Uri> task = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+
                         task.addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
                             public void onSuccess(Uri uri) {
+
                                 String url = uri.toString();
-                                DatabaseReference myRef = database.getReference();
+
+                                DatabaseReference myRef = mDatabase.getReference();
+
                                 myRef.child(fileName).setValue(url)
                                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                                             @Override
                                             public void onComplete(@NonNull Task<Void> task) {
+
                                                 if (task.isSuccessful()) {
+
                                                     Toast.makeText(Curriculo.this, "File is uploaded", Toast.LENGTH_SHORT).show();
 
+                                                    progressDialog.dismiss();
+
                                                 } else {
+
                                                     Toast.makeText(Curriculo.this, "File upload failed", Toast.LENGTH_SHORT).show();
 
+                                                    progressDialog.dismiss();
+
                                                 }
+
                                             }
                                         });
+
                             }
                         });
                     }
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(Curriculo.this, "Erro no upload", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(Curriculo.this, "ERROR! Something went wrong try again.", Toast.LENGTH_SHORT).show();
+
+                progressDialog.dismiss();
 
             }
         }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                int currentProgress = (int)(100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
-//                progressDialog.setProgress(currentProgress);
+
+                int currentProgress = (int) (100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+
+                progressDialog.setProgress(currentProgress);
 
             }
         });
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 9 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            selectPdf();
-        }
-        else
-            Toast.makeText(Curriculo.this,"Por favor conceda o acesso aos seus arquivos",Toast.LENGTH_SHORT).show();
-    }
-
-    private void selectPdf() {
-        Intent intent = new Intent();
-        intent.setType("application/pdf");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,86);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 86 && resultCode == RESULT_OK && data != null) {
-            pdfUri = data.getData();
-            notification.setText("Arquivo selecionado: " + data.getData().getLastPathSegment());
-        } else {
-            Toast.makeText(Curriculo.this, "Por favor escolha seu arquivo", Toast.LENGTH_SHORT).show();
-        }
-    }
 
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 9965 && resultCode == RESULT_OK && data != null) {
+
+            pdfUri = data.getData();
+
+            mFileStatus.setText("Selected File: " + data.getData().getLastPathSegment());
+
+        } else {
+
+            Toast.makeText(this, "Please select a file!", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
 }

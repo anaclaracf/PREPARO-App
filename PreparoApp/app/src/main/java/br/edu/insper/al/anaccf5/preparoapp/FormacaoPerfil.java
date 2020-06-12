ab5206b5f95b80ac9a5fddfd0999e49581538f57
William Silva <williamars@al.insper.edu.br>
2020-06-12 17:54:59 -0300

    Add NavBar em InfoPessoais e Curriculos

--- PreparoApp/app/src/main/java/br/edu/insper/al/anaccf5/preparoapp/FormacaoPerfil.java
+++ PreparoApp/app/src/main/java/br/edu/insper/al/anaccf5/preparoapp/FormacaoPerfil.java
@@ -1,11 +1,19 @@
 package br.edu.insper.al.anaccf5.preparoapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -16,7 +24,7 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import javax.annotation.Nullable;
 
-public class FormacaoPerfil extends AppCompatActivity {
+public class FormacaoPerfil extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView le_curso,le_insituicao, le_processograd, le_situacao, le_name;
 
@@ -24,6 +32,10 @@ public class FormacaoPerfil extends AppCompatActivity {
     private FirebaseAuth auth;
 
     String userid;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,19 @@ public class FormacaoPerfil extends AppCompatActivity {
         fstore = FirebaseFirestore.getInstance();
 
         userid = auth.getCurrentUser().getUid();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_perfil);
+
 
         final DocumentReference documentReference = fstore.collection("candidatos").document(userid);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
@@ -95,5 +120,45 @@ public class FormacaoPerfil extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_perfil:
+                break;
+            case R.id.nav_vagas:
+                Intent intent = new Intent(FormacaoPerfil.this, Vagas.class);
+                startActivity(intent);
+                FormacaoPerfil.this.onPause();
+                break;
+            case R.id.nav_sair:
+                Intent i = new Intent(FormacaoPerfil.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                finish();
+                break;
+            case R.id.nav_sobre:
+                Intent intent1 = new Intent(FormacaoPerfil.this, SobreNos.class);
+                startActivity(intent1);
+                FormacaoPerfil.this.onPause();
+                break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
